3d5c134cc4a162b4208b8249e9ead4fb6893f4d2
Ahmad N. Raja <ar@penneo.com>
2015-06-26 09:56:42 +0200

    Reopen the most recently killed buffer in emacs

--- emacs.d/util.el
+++ emacs.d/util.el
@@ -61,6 +61,25 @@
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
 
+
+;; Open the most recently killed buffer
+;; http://emacs.stackexchange.com/questions/3330/how-to-reopen-just-killed-buffer-like-c-s-t-in-firefox-browser
+(defvar killed-file-list nil
+  "List of recently killed files.")
+(defun add-file-to-killed-file-list ()
+  "If buffer is associated with a file name, add that file to the
+`killed-file-list' when killing the buffer."
+  (when buffer-file-name
+    (push buffer-file-name killed-file-list)))
+(add-hook 'kill-buffer-hook #'add-file-to-killed-file-list)
+(defun reopen-killed-file ()
+  "Reopen the most recently killed file, if one exists."
+  (interactive)
+  (when killed-file-list
+    (find-file (pop killed-file-list))))
+(define-key global-map (kbd "M-T") 'reopen-killed-file)
+
+
 (defun anr-shell (buffer)
   "Opens a new shell buffer where the given buffer is located."
   (interactive "sBuffer: ")
