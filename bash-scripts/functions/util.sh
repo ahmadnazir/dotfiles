a3514b314466866ea5d1fc624c38860e250a0cca
Ahmad N. Raja <ar@penneo.com>
2015-07-14 14:03:45 +0200

    Convert seconds to h:m:s

--- bash-scripts/functions/util.sh
+++ bash-scripts/functions/util.sh
@@ -8,3 +8,13 @@ function swap()
     mv "$2" "$1"
     mv $TMPFILE "$2"
 }
+
+# Time
+
+# http://stackoverflow.com/a/12199798/1589512
+convertsecs() {
+ ((h=${1}/3600))
+ ((m=(${1}%3600)/60))
+ ((s=${1}%60))
+ printf "%02d:%02d:%02d\n" $h $m $s
+}
