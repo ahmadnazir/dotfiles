033ffb3838ecaa0005d8a76e16981c70051a6c57
Ahmad N. Raja <ar@penneo.com>
2015-08-12 00:29:35 +0200

    Specifying the command as the argument
    Previously the directory was being specified which isn't needed

--- bash-scripts/functions/docker.sh
+++ bash-scripts/functions/docker.sh
@@ -11,10 +11,11 @@ docker-clean-exited-containers() {
     docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm
 }
 
-# Get an interactive docker environment mapped to the current working directory
+# Docker interactive: Get an interactive docker environment mapped to
+# the current working directory
 #
-# Usage: 'dt haskell', which will run a container for the haskell image
-dt () {
+# Usage: 'dint haskell', which will run a container for the haskell image
+dint () {
 
 	# image
 	if [[ -z $1 ]]; then
@@ -25,9 +26,15 @@ dt () {
 
 	# shared directory
 	local dir='/shared';
-	if [[ -n $2 ]]; then
-		dir=$2;
-	fi
+	# @todo: use a switch to set the shared dir name
+	# if [[ -n $2 ]]; then
+	# 	dir=$2;
+	# fi
 
-	docker run -it --rm -w $dir -v `pwd`:$dir $image
+	local cmd=''
+	if [[ -n $2 ]]; then
+		cmd=$2;
+	fi
+	
+	docker run -it --rm -w $dir -v `pwd`:$dir $image $cmd
 }
