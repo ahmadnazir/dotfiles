80a553a8f23239e104c4f60f210f2551d8b8753c
Ahmad N. Raja <ar@penneo.com>
2015-08-12 00:26:08 +0200

    Hacked phpunit so that it works with docker
    Need to come up with a better solution. The current hack isn't good
    enough for a pull request

--- emacs.d/dev.el
+++ emacs.d/dev.el
@@ -1,3 +1,36 @@
-;; Load development files
+;; PHP unit
 (load-file "~/code/forks/nlamirault/phpunit.el/phpunit.el")
+;;
+;; Helpers
+;;
+(defun an-phpunit-get-program ()
+  "Return the docker command to launch unit test"
+  (progn ()
+		 (if (get-buffer "**phpunit-docker**") (kill-buffer "**phpunit-docker**"))
+		 ;; @hack: there can be multiple containers running for the same image
+		 ;; Get the container id
+		 (call-process-shell-command "docker ps | grep 'penneo/webapp' | awk '{printf $1}'" nil "**phpunit-docker**")
+		 (s-concat "docker exec " (an-buffer-string "**phpunit-docker**") " phpunit")
+		 )
+  )
+;;
+;; Overrides
+;;
+(defun phpunit-get-program (args)
+  "Return the command to launch unit test.
+`ARGS' corresponds to phpunit command line arguments."
+  ;; (s-concat phpunit-program " -c "
+  (s-concat (an-phpunit-get-program) " -c "
+	    (phpunit-get-root-directory)
+	    phpunit-configuration-file
+	    args))
+
+
+
+;; Rest Client
 (load-file "~/code/forks/pashky/restclient.el/restclient.el")
+
+;; Extend Emacs with Haskell
+(load-file "~/code/forks/knupfer/haskell-emacs/haskell-emacs.el")
+(require 'haskell-emacs)
+;; (haskell-emacs-init) ;; required for compiling. Better use it interactively
