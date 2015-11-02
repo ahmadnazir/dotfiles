b724b35f987105a62f5c06dd80a73b08d92c5667
Ahmad N. Raja <ar@penneo.com>
2015-11-02 12:42:23 +0100

    Support for 3 screens

--- emacs.d/visual.el
+++ emacs.d/visual.el
@@ -51,6 +51,10 @@
                                                ; window, not occupy
                                                ; whole other window
 
+;; Automatically shrink the window that is out of focus
+;; @see: https://truongtx.me/2014/11/15/auto-resize-windows-by-golden-ratio-in-emacs/
+;; (golden-ratio-mode 1)
+
 ;; -------------
 ;; GUI MODE ONLY
 ;; -------------
@@ -117,3 +121,32 @@
 	  (previous-buffer)
 	  (kill-buffer buffer)))
 (global-set-key (kbd "M-W") 'anr-kill-this-buffer)
+
+;; Toggle between two windows setup horizontally or vertically
+;; Only works for 2 exactly two windows
+;; @see http://emacswiki.org/emacs/ToggleWindowSplit
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
