ab5dd4067e0787a00028c0e7e4acffd7b36168eb
Ahmad N. Raja <ar@penneo.com>
2017-04-16 14:06:49 +0200

    Date function in local util package

--- spacemacs/private/anr/local/util/util.el
+++ spacemacs/private/anr/local/util/util.el
@@ -8,4 +8,11 @@
     (insert-file-contents filePath)
     (buffer-string)))
 
+;;;###autoload
+(defun util/date (arg)
+  (interactive "P")
+  (insert (if arg
+              (format-time-string "%d.%m.%Y")
+            (format-time-string "%Y-%m-%d"))))
+
 (provide 'util)
