29b8938231e7351fc466f725b935c0845f6d2388
Ahmad N. Raja <ar@penneo.com>
2018-05-07 20:57:30 +0200

    Running emacs daemons

--- symlinks.sh
+++ symlinks.sh
@@ -11,7 +11,12 @@ sudo ln -s ~/.cabal/bin/cabal /usr/bin/cabal
 # Emacs
 #
 # Server
-ln -s ~/.bash-scripts/e.sh ~/bin/e
+ln -s ~/.bash-scripts/init-emacs-daemons.sh ~/bin/init-emacs-daemons
+
 #
 # Client
-ln -sf ~/.bash-scripts/client.sh ~/bin/client
+ln -s ~/.bash-scripts/emacs-primary.sh ~/bin/emacs-primary
+ln -s ~/.bash-scripts/emacs-secondary.sh ~/bin/emacs-secondary
+
+# Cask
+sudo ln -s ~/.cask/bin/cask /usr/bin/cask
