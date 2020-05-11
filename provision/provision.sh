a7c280c738a272ef690d205e39b1e6658899dd46
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-05-11 11:17:17 +0200

    Post provisioning commands

--- provision/provision.sh
+++ provision/provision.sh
@@ -7,3 +7,8 @@ sudo ansible-playbook playbook.yml -vvv
 
 # Default shell (requires logout)
 chsh -s $(which zsh)
+
+# Docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker # This only works for current shell / Restart to make sure groups are re-evaluated
