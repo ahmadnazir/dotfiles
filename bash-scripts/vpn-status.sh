b1dac435530a6486b00774f1f05aefc607f2417c
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-05-13 20:31:58 +0200

    Check vpn status

--- bash-scripts/vpn-status.sh
+++ bash-scripts/vpn-status.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# function vpn-status()
+# {
+#     ifconfig tun0 > /dev/null &&
+#         if [[ $? == 0 ]]
+#         then
+#             echo -e " :)"
+#         fi ||
+#             echo -e ":'("
+# }
+
 function vpn-status()
 {
-    ifconfig tun0 > /dev/null &&
-        if [[ $? == 0 ]]
-        then
-            echo -e " :)"
-        fi ||
-            echo -e ":'("
+    if [[ $(routel | grep tun0) ]]; then
+        echo -e "✔"
+    else
+        echo -e "✖"
+    fi
 }
 
 vpn-status
