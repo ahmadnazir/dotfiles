8ad87fa3fc90fc29ce7e312074585443832cf296
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-05-28 10:35:41 +0200

    Manual installation of clojure

--- provision/packages.sh
+++ provision/packages.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Spotify
 sudo snap install spotify
+
+# Clojure
+cd /tmp
+curl -O https://download.clojure.org/install/linux-install-1.10.1.536.sh
+chmod +x linux-install-1.10.1.536.sh
+sudo ./linux-install-1.10.1.536.sh
