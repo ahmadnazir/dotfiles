f6636a18385d009134837e8b539bbf07cc4bb04b
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-05-30 20:14:32 +0200

    Emacs function to make current file executable

--- spacemacs/private/anr/local/util/util.el
+++ spacemacs/private/anr/local/util/util.el
@@ -15,4 +15,10 @@
               (format-time-string "%d.%m.%Y")
             (format-time-string "%Y-%m-%d"))))
 
+;;;###autoload
+(defun util/make-executable ()
+  (interactive)
+  (shell-command (concat "chmod u+x " (shell-quote-argument buffer-file-name) " && echo Updated '" buffer-file-name "'")))
+
+
 (provide 'util)
