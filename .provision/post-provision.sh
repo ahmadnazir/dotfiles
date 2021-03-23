3d68b93760cc474e8ccbfc7001080bfd733219d4
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2021-03-23 20:58:51 +0100

    Synced micro changes

--- .provision/post-provision.sh
+++ .provision/post-provision.sh
@@ -2,10 +2,48 @@
 
 # Post install
 systemctl enable --now iwd
-systemctl enable --now systemd-notify
+systemctl enable --now docker
+systemctl enable --now bluetooth
+
+# TODO: don't remember if we need both?
+systemctl enable --user --now pulseaudio
+systemctl enable --user --now pulseaudio.socket
+
+# Bluetooth
+# ---------
+sudo rfkill unlock bluetooth
+
+# Add the following to /etc/pulse/system.pa
+# ```
+# load-module module-bluetooth-policy
+# load-module module-bluetooth-discover
+# ```
+systemctl enable --now bluetooth
+
+# Pair the device
+# ```
+# - power on
+# - discoverable on
+# - scan on # wait until the device is found (keep it in pair mode)
+# - scan off
+# - connect with device
+# - trust device # optional
+# ```
+
+
+# TODO: docker groups
+
+# ------------
+# User archive
+# ------------
 
 # TODO: use ansible for yay
+
+# system level
 yay -S ttf-symbola-free
 yay -S silver-searcher-git
 yay -S mcrypt
 yay -S xscreensaver-arch-logo
+
+# other
+yay -S nvm
