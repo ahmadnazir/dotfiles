d84c383bf83156d1e41fc8fa771f99d223306773
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2022-01-24 19:34:04 +0100

    Update scribbles functions

--- .functions/scribbles.sh
+++ .functions/scribbles.sh
@@ -2,20 +2,7 @@
 IMAGE=suttang/sphinx-rtd-theme
 DIR=~/code/me/scribbles
 
-function -scribbles-publish () {
-    cd $DIR > /dev/null
-
-    text-info 'SCRIBBLES :: Publishing to https://ahmadnazir.github.io/scribbles'
-
-    git push origin `git subtree split --prefix build/html source`:master --force
-
-    text-info 'SCRIBBLES :: Publishing man pages'
-
-    sudo cp build/man/scribbles.7 /usr/share/man/man7/
-    cd - > /dev/null
-}
-
-function -scribbles-clean () {
+function scribbles-clean () {
     cd $DIR/build > /dev/null
     mv html html_
     mkdir html
@@ -34,8 +21,27 @@ function -scribbles-compile () {
            $IMAGE make html man
 }
 
+function -scribbles-publish () {
+    cd $DIR > /dev/null
+
+    text-info 'SCRIBBLES :: Updating git repo https://github.com/ahmadnazir/scribbles'
+
+    git push
+
+    text-info 'SCRIBBLES :: Publishing site https://ahmadnazir.github.io/scribbles'
+
+    git push origin `git subtree split --prefix build/html source`:master --force
+
+    text-info 'SCRIBBLES :: Publishing man pages locally'
+
+    sudo cp build/man/scribbles.7 /usr/share/man/man7/
+    cd - > /dev/null
+}
+
 function scribbles-publish () {
-    # Can publish?
+    -scribbles-compile
+
+    # Detect un-committed changes
     cd $DIR > /dev/null
     git status | grep 'working tree clean'
     RESULT=$?
@@ -45,8 +51,6 @@ function scribbles-publish () {
 
         git diff
     else
-        -scribbles-clean
-        -scribbles-compile
         -scribbles-publish
     fi
     cd - > /dev/null
