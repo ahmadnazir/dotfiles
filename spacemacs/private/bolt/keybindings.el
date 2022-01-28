6fcf84f8e117b7f3d60cdde69f91d53ed1f10f2b
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2022-01-28 22:23:42 +0100

    Removing submodules

--- spacemacs/private/bolt/keybindings.el
+++
@@ -1,8 +0,0 @@
-(spacemacs/declare-prefix "ox" "Execute with bolt")
-
-;; Execute
-(spacemacs/set-leader-keys "ox." 'bolt--execute)
-(spacemacs/set-leader-keys "oxx" 'bolt--execute-cmd)
-
-;; setup
-(spacemacs/set-leader-keys "oxd" 'bolt--execute-default-cmd)
--- spacemacs/private/sos/keybindings.el
+++
@@ -1,6 +0,0 @@
-(add-hook 'sql-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "<C-return>") 'exsqlaim-mode--send)
-             ;; (sql-set-sqli-buffer-generally)
-             ))
-
